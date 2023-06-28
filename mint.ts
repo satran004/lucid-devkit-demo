ef456319c03895e322314b6f4d11d513076e4122
Satya <satran004@gmail.com>
2023-06-28 12:56:01 +0800

    Address added

--- mint.ts
+++ mint.ts
@@ -6,6 +6,7 @@ const lucid = await Lucid.new(
   "Preprod",
 );
 
+//addr_test1qzx9hu8j4ah3auytk0mwcupd69hpc52t0cw39a65ndrah86djs784u92a3m5w475w3w35tyd6v3qumkze80j8a6h5tuqq5xe8y
 lucid.selectWalletFromSeed('damp wish scrub sentence vibrant gauge tumble raven game extend winner acid side amused vote edge affair buzz hospital slogan patient drum day vital');
 
 const { paymentCredential } = lucid.utils.getAddressDetails(
--- pay.ts
+++ pay.ts
@@ -1,12 +1,13 @@
 import { Blockfrost, Lucid } from "https://deno.land/x/lucid@0.9.8/mod.ts"; Deno
 
 const lucid = await Lucid.new(
- // new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", 
+ // new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0",
  new Blockfrost("http://localhost:8080/api/v1",
   ""),
   "Preprod",
 );
 
+//addr_test1qzx9hu8j4ah3auytk0mwcupd69hpc52t0cw39a65ndrah86djs784u92a3m5w475w3w35tyd6v3qumkze80j8a6h5tuqq5xe8y
 lucid.selectWalletFromSeed('damp wish scrub sentence vibrant gauge tumble raven game extend winner acid side amused vote edge affair buzz hospital slogan patient drum day vital');
 
 const tx = await lucid.newTx()
